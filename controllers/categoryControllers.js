01c45bccd82596218d7371f95a0907d7a3022020
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-11-27 20:24:21 -0300

    Edit Category Controller Added

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -2,29 +2,43 @@ const pool = require('../config/database')
 
 const categoryControllers = {
     createCategory: async (req, res) => {
-        const { name, type, image } = req.body
+        const { name, nombre, type, image } = req.body
         try {
             await pool.query('CREATE TABLE IF NOT EXISTS categories (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, nombre VARCHAR(20) NOT NULL, type VARCHAR(10) NOT NULL, image VARCHAR(50) NOT NULL)')
-            let category = await pool.query(`INSERT INTO categories VALUES (NULL, '${name}', '${type}', '${image}')`)
-            res.json({ success: true, response: category })
+            await pool.query(`INSERT INTO categories VALUES (NULL, '${name}', '${nombre}', '${type}', '${image}')`)
+            res.json({ success: true })
         } catch (error) {
             res.json({ success: false, error: error.message })
         }
     },
     getCategory: async (req, res) => {
         try {
-            let category = pool.query(`SELECT * FROM categories WHERE id='${req.params.id}'`)
+            let category = await pool.query(`SELECT * FROM categories WHERE id='${req.params.id}'`)
             res.json({ success: true, response: category })
         } catch (error) {
             res.json({ success: false, error: error.message })
         }
     },
     editCategory: async (req, res) => {
-
+        const values = Object.entries(req.body)
+        let query = ''
+        values.forEach((value, index) => {
+            if (index === values.length - 1) {
+                query += `${value[0]}="${value[1]}"`
+            } else {
+                query += `${value[0]}="${value[1]}", `
+            }
+        })
+        try {
+            await pool.query(`UPDATE categories SET ${query} WHERE id=${req.params.id}`)
+            res.json({ success: true })
+        } catch (error) {
+            res.json({ success: false, error: error.message })
+        }
     },
     deleteCategory: async (req, res) => {
         try {
-            await pool.query(`DELETE FROM cateogies WHERE id='${req.params.id}'`)
+            await pool.query(`DELETE FROM categories WHERE id='${req.params.id}'`)
             res.json({ success: true })
         } catch (error) {
             res.json({ success: false, error: error.message })
@@ -33,7 +47,7 @@ const categoryControllers = {
     getCategories: async (req, res) => {
         try {
             let categories = await pool.query('SELECT * from categories')
-            res.json({ success: trualle, response: categories })
+            res.json({ success: true, response: categories })
         } catch (error) {
             res.json({ success: false, error: error.message })
         }
